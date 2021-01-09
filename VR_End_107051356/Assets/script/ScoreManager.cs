845e12d811c94e7a71c8ab96dd35eba0a3c12151
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2021-01-10 04:23:42 +0800

    1/10 還未放音效

---
+++ VR_End_107051356/Assets/script/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "乒乓球")
+        {
+            AddScore();
+        }
+
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 1;
+        }
+    }
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "Score : " + score;
+    }
+}
